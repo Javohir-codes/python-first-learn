7cad2f36b7f49304c3b0528e55f9b872894e91c7
Javohir-codes <jo2011baxti@gmail.com>
2025-10-08 20:15:34 +0500

    first lesson of python

---
+++ lesson.py
@@ -0,0 +1,16 @@
+a = 5
+number = 10
+b = "Javohir"
+c = False
+d = 3.14
+
+print(a+number) # плюс
+print(a-number) # минус
+print(a*number) # умножение
+print(a/number) # деление
+print(a//number) # деление без остатков
+print(a**number) # степень
+print(a%number) # остаток
+print(a==number) # сравнение
+print(a>number) # сравнение
+print(a<number) # сравнение
